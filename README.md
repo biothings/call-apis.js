de735dca64ad89c179858b7b8ca72bb5e714a321
jiwen xin <kevinxin@scripps.edu>
2020-06-26 16:43:43 -0700

    initial commiit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # call-apis.js
 A nodejs module to make API calls for BioThings Explorer
+
+[![Build Status](https://travis-ci.com/kevinxin90/call-apis.js.svg?branch=master)](https://travis-ci.com/kevinxin90/call-apis.js)
+[![Coverage Status](https://coveralls.io/repos/github/kevinxin90/call-apis.js/badge.svg?branch=master)](https://coveralls.io/github/kevinxin90/call-apis.js?branch=master)
---
+++ index.js
@@ -0,0 +1,59 @@
+const axios = require("axios");
+const qb = require("./query_builder");
+const tf = require("@biothings-explorer/api-response-transform");
+const resolver = require("@biothings-explorer/id_resolver");
+
+module.exports = class APIQueryDispathcer {
+
+    constructor(edges) {
+        this.edges = edges;
+    }
+
+    /**
+     * Make API calls and parse the response.
+     */
+    query = async () => {
+        this.queryResult = await Promise.allSettled(this.edges.map(edge => {
+            let qbo = new qb(edge);
+            return axios(qbo.config)
+                .then(res => ({
+                    response: res.data,
+                    edge: edge
+                }))
+                .then(res => {
+                    let tf_obj = new tf(res);
+                    return tf_obj.transform();
+                })
+                .catch(error => {
+                    console.log(error);
+                    return undefined;
+                });
+        }));
+        this.merge();
+        await this.annotate();
+    }
+
+    merge = () => {
+        this.result = [];
+        this.queryResult.map(res => {
+            if (!(res.value === undefined)) {
+                this.result = [...this.result, ...res.value];
+            }
+        });
+    }
+
+    annotate = async () => {
+        let output_ids = this.result.map(item => item.$output);
+        let res = await resolver(output_ids);
+        this.result.map(item => {
+            if (item.$output in res) {
+                item.$output_id_mapping = {
+                    resolved: res[item.$output],
+                    original: item.$output
+                };
+                item.$output = res[item.$output].id.identifier;
+            }
+        })
+    }
+
+}
