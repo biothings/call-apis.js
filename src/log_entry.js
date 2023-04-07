b4e345be501c0f371f007f82b9c4bc0efc5e1b06
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-07 12:44:08 -0400

    feat: support Bull logging

--- src/log_entry.js
+++ src/log_entry.js
@@ -1,22 +1,29 @@
-const _ = require('lodash');
+const _ = require("lodash");
 
 module.exports = class LogEntry {
+  constructor(level = "DEBUG", code = null, message = null, data = null) {
+    this.level = level;
+    this.message = message;
+    this.code = code;
+    this.data = data;
+  }
 
-    constructor(level = "DEBUG", code = null, message = null, data = null) {
-        this.level = level;
-        this.message = message;
-        this.code = code;
-        this.data = data;
+  getLog() {
+    const log = {
+      timestamp: new Date().toISOString(),
+      level: this.level,
+      message: this.message,
+      code: this.code,
     }
-
-    getLog() {
-        return {
-            timestamp: new Date().toISOString(),
-            level: this.level,
-            message: this.message,
-            code: this.code,
-            data: this.data,
-            toJSON() { return _.omit(this, ['data', 'toJSON']); }
-        }
+    if (global.job) {
+      global.job.log(JSON.stringify(log, undefined, 2));
     }
-}
+    return {
+      ...log,
+      data: this.data,
+      toJSON() {
+        return _.omit(this, ["data", "toJSON"]);
+      },
+    };
+  }
+};
