70e464bb2d12655a1f2271302ede6a96a362c1dd
tokebe <43009413+tokebe@users.noreply.github.com>
2023-12-07 12:33:57 -0500

    fix: rate limit automatic fallback from redis

--- src/rate_limiter.ts
+++ src/rate_limiter.ts
@@ -1,9 +1,13 @@
 import BaseQueryBuilder from "./builder/base_query_builder";
 import { RedisClient } from "./types";
+import Debug from "debug";
+const debug = Debug("bte:call-apis:query");
 
 // Default rate limit of 100 queries per second, which shouldn't ever be reached
 // But just in case, this should keep multiple instances from overloading anyone
-const DEFAULT_RATE_LIMIT = parseInt(process.env.DEFAULT_API_RATE_LIMIT ?? "6000");
+const DEFAULT_RATE_LIMIT = parseInt(
+  process.env.DEFAULT_API_RATE_LIMIT ?? "6000",
+);
 
 export default class RateCounter {
   redisClient: RedisClient;
@@ -19,22 +23,52 @@ export default class RateCounter {
     const apiKey = `APIUsageCount:${query.APIEdge.association.api_name}`;
     const limit =
       query.APIEdge.association["x-trapi"]?.rate_limit ?? DEFAULT_RATE_LIMIT;
+    let usage: string;
     if (this.redisClient.clientEnabled) {
-      const usage = (await this.redisClient.client.getTimeout(apiKey)) ?? "0";
-      return parseInt(usage) >= limit;
+      try {
+        usage = await new Promise((resolve, reject) => {
+          setTimeout(() => reject(), 500);
+          this.redisClient.client.getTimeout(apiKey).then(
+            result => resolve(result),
+            error => reject(error),
+          );
+        });
+      } catch (error) {
+        debug("Redis failed to respond to APIUsageCount query in time.");
+      }
     }
-    return (this.usage[apiKey] ?? 0) > limit;
+    if (!usage) {
+      usage = `${this.usage[apiKey] ?? 0}`;
+    }
+    return parseInt(usage) >= limit;
   }
 
   async count(query: BaseQueryBuilder): Promise<void> {
     const apiKey = `APIUsageCount:${query.APIEdge.association.api_name}`;
     if (this.redisClient.clientEnabled) {
-      await this.redisClient.client.incrTimeout(apiKey);
-      await this.redisClient.client.expireTimeout(apiKey, 60);
-      setTimeout(async () => {
-        await this.redisClient.client.decrTimeout(apiKey);
-      });
-      return;
+      try {
+        await new Promise<void>((resolve, reject) => {
+          setTimeout(() => reject(), 500);
+          this.redisClient.client.incrTimeout(apiKey).then(
+            () =>
+              this.redisClient.client.expireTimeout(apiKey, 60).then(
+                () => {
+                  setTimeout(() => {
+                    this.redisClient.client.decrTimeout(apiKey).then(
+                      () => null,
+                      error => reject(error),
+                    );
+                  });
+                  resolve();
+                },
+                error => reject(error),
+              ),
+            error => reject(error),
+          );
+        });
+      } catch {
+        debug("Redis failed to respond to APIUsageCount query in time.");
+      }
     }
     if (typeof this.usage[apiKey] == "undefined") this.usage[apiKey] = 0;
     this.usage[apiKey] += 1;
